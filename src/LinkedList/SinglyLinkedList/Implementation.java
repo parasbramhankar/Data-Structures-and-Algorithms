721e989f51037b1b588e67bc001b5526a0bd13f4
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-17 09:35:19 +0530

    LinkedList update

--- src/LinkedList/SinglyLinkedList/Implementation.java
+++ src/LinkedList/SinglyLinkedList/Implementation.java
@@ -1,7 +1,65 @@
 package LinkedList.SinglyLinkedList;
 
-public class Implementation {
-    public static void main(String[] args) {
 
+import java.util.Scanner;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
+
+
+public class Implementation {
+    static Node head;
+    static Node addDataInTheLinkedList(int []data){
+
+        Node node=new Node(data[0]);
+        Node temp=head;
+
+        if(head==null){
+            head=node;
+            temp=node;
+        }else{
+            while(temp.next!=null){
+                temp=temp.next;
+            }
+        }
+
+        for(int i=1;i<data.length;i++){
+            temp.data=data[i];
+            temp=temp.next;
+        }
+
+        return head;
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter number of element you want to insert into the linked list: ");
+        int n=scanner.nextInt();
+
+        int []arr=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]= scanner.nextInt();
+        }
+
+        Node head=addDataInTheLinkedList(arr);
+
+        displayList(head);
+    }
+    static void displayList(Node head){
+        Node temp=head;
+
+        while(temp.next!=null){
+            System.out.println(temp.data+" ");
+        }
     }
 }
