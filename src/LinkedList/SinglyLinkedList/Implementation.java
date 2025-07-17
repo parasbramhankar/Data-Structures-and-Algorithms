163d154a91abd9c68319252144803c1f61912ae4
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-17 09:49:48 +0530

    LinkedList update

--- src/LinkedList/SinglyLinkedList/Implementation.java
+++ src/LinkedList/SinglyLinkedList/Implementation.java
@@ -1,65 +1,60 @@
 package LinkedList.SinglyLinkedList;
 
-
 import java.util.Scanner;
 
-class Node{
+class Node {
     int data;
     Node next;
 
     Node(int data){
-        this.data=data;
-        this.next=null;
+        this.data = data;
+        this.next = null;
     }
 }
 
 public class Implementation {
     static Node head;
-    static Node addDataInTheLinkedList(int []data){
 
-        Node node=new Node(data[0]);
-        Node temp=head;
+    static Node addDataInTheLinkedList(int[] data){
+        if (data.length == 0) return null;
 
-        if(head==null){
-            head=node;
-            temp=node;
-        }else{
-            while(temp.next!=null){
-                temp=temp.next;
-            }
-        }
+        // First node
+        head = new Node(data[0]);
+        Node temp = head;
 
-        for(int i=1;i<data.length;i++){
-            temp.next=new Node(data[i]);
-            temp=temp.next;
+        // Remaining nodes
+        for (int i = 1; i < data.length; i++) {
+            temp.next = new Node(data[i]);
+            temp = temp.next;
         }
 
         return head;
-
     }
+
+    static void displayList(Node head){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println(); // Clean line after printing all elements
+    }
+
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter number of element you want to insert into the linked list: ");
-        int n=scanner.nextInt();
+        System.out.println("Enter number of elements you want to insert into the linked list: ");
+        int n = scanner.nextInt();
 
-        int []arr=new int[n];
-
+        int[] arr = new int[n];
         System.out.println("Enter array elements: ");
-
-        for(int i=0;i<n;i++){
-            arr[i]= scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
         }
 
-        Node head=addDataInTheLinkedList(arr);
+        Node head = addDataInTheLinkedList(arr);
+
         System.out.println("The linked list: ");
         displayList(head);
-    }
-    static void displayList(Node head){
-        Node temp=head;
-        while(temp.next!=null){
-            System.out.println(temp.data+" "+temp.next);
-            temp=temp.next;
-        }
     }
 }
