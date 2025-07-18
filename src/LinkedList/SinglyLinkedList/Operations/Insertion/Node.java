b27088748a55988748efa49f7f1caaddc06505c1
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-18 10:21:52 +0530

    LinkedList update

---
+++ src/LinkedList/SinglyLinkedList/Operations/Insertion/Node.java
@@ -0,0 +1,11 @@
+package LinkedList.SinglyLinkedList.Operations.Insertion;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
