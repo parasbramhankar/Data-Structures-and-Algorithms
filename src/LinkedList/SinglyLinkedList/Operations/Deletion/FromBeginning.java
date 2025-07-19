05ba2063b7690e632b11a267a1c022bb5c4ca351
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-19 19:56:03 +0530

    LinkedList update

---
+++ src/LinkedList/SinglyLinkedList/Operations/Deletion/FromBeginning.java
@@ -0,0 +1,42 @@
+package LinkedList.SinglyLinkedList.Operations.Deletion;
+
+public class FromBeginning {
+
+    static Node removeFromBeginning(Node head){
+        if(head == null) {
+            System.out.println("List is already empty.");
+            return null;
+        }
+        return head.next;
+    }
+
+    static void display(Node head){
+
+        Node temp=head;
+
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+      Node head=new Node(10);
+      head.next=new Node(20);
+      head.next.next=new Node(30);
+      head.next.next.next=new Node(40);
+      head.next.next.next.next=new Node(50);
+
+
+      System.out.println("Linked List Before Deletion: ");
+      display(head);
+
+      head=removeFromBeginning(head);
+
+      System.out.println("Linked List after Deletion: ");
+      display(head);
+
+    }
+
+}
