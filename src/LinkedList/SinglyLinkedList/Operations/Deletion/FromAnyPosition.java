c7c163ffd697f2078155c95569316d6cda75f901
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-19 19:57:42 +0530

    LinkedList update

---
+++ src/LinkedList/SinglyLinkedList/Operations/Deletion/FromAnyPosition.java
@@ -0,0 +1,53 @@
+package LinkedList.SinglyLinkedList.Operations.Deletion;
+
+import java.util.Scanner;
+
+public class FromAnyPosition {
+
+
+    static void removeFromAnyPosition(Node head,int pos){
+         if(head==null){
+             System.out.println("List is Empty");
+             return;
+         }
+
+
+    }
+
+
+    static void display(Node head){
+
+        Node temp=head;
+
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head=new Node(10);
+        head.next=new Node(20);
+        head.next.next=new Node(30);
+        head.next.next.next=new Node(40);
+        head.next.next.next.next=new Node(50);
+
+
+        System.out.println("Linked List Before Deletion: ");
+        display(head);
+
+        System.out.println("Enter the  position to delete the node: ");
+
+
+        int pos=new Scanner(System.in).nextInt();
+
+        removeFromAnyPosition(head,pos);
+
+        System.out.println("Linked List after Deletion: ");
+        display(head);
+
+    }
+
+
+}
