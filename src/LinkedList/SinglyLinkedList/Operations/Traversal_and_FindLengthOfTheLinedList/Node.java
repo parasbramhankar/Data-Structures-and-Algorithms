33a51261d2448ad007ca7ca6817726f4571cfd22
parasbramhankar <parasbramhankar02@gmail.com>
2025-07-19 20:15:13 +0530

    LinkedList update

---
+++ src/LinkedList/SinglyLinkedList/Operations/Traversal_and_FindLengthOfTheLinedList/Node.java
@@ -0,0 +1,11 @@
+package LinkedList.SinglyLinkedList.Operations.Traversal_and_FindLengthOfTheLinedList;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
