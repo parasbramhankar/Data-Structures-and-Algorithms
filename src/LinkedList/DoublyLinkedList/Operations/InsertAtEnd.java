56b5d6c5f808c439ec9c65b6dc5cb8f46cafa019
parasbramhankar <parasbramhankar.in@gmail.com>
2025-08-04 08:59:39 +0530

    LinkedList update

---
+++ src/LinkedList/DoublyLinkedList/Operations/InsertAtEnd.java
@@ -0,0 +1,54 @@
+package LinkedList.DoublyLinkedList.Operations;
+
+public class InsertAtEnd {
+
+    static Node InsertAtEndFun(Node head,int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return head;
+        }
+
+        Node temp=head;
+
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+
+        temp.next=newNode;
+        newNode.prev=temp;
+
+        return head;
+    }
+    static void displayDLL(Node head) {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data);
+            if (temp.next != null) System.out.print("->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head=new Node(10);
+        head.next=new Node(20,null,head);
+        head.next.next=new Node(30,null,head.next);
+        head.next.next.next=new Node(40,null,head.next.next);
+        head.next.next.next.next=new Node(50,null,head.next.next.next);
+
+        System.out.println("Before Insertion: ");
+        displayDLL(head);
+
+        head=InsertAtEndFun(head,5);
+
+        System.out.println("After Insertion: ");
+        displayDLL(head);
+
+    }
+}
