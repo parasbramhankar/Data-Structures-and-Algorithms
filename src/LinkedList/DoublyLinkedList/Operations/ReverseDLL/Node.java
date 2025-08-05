b1e19dd1cbb68f885180e113c4377814881a1ae6
parasbramhankar <parasbramhankar.in@gmail.com>
2025-08-05 09:18:55 +0530

    Doubly LinkedList update

---
+++ src/LinkedList/DoublyLinkedList/Operations/ReverseDLL/Node.java
@@ -0,0 +1,21 @@
+package LinkedList.DoublyLinkedList.Operations.ReverseDLL;
+
+public class Node {
+
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data, Node next, Node prev){
+        this.data=data;
+        this.next=next;
+        this.prev=prev;
+    }
+
+    Node(int data){
+        this.data=data;
+        this.next=null;
+        this.prev=null;
+    }
+
+}
