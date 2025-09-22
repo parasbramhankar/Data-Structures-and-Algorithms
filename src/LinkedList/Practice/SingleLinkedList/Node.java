f0a8a03287a64c3505b991c079ff371844b204ba
parasbramhankar <parasbramhankar.in@gmail.com>
2025-09-22 08:51:24 +0530

    Linked List commit

---
+++ src/LinkedList/Practice/SingleLinkedList/Node.java
@@ -0,0 +1,15 @@
+package LinkedList.Practice.SingleLinkedList;
+
+public class Node {
+    int data;
+    Node next;
+
+    Node(int data,Node next){
+        this.data=data;
+        this.next=next;
+    }
+
+    Node(int data){
+        this.data=data;
+    }
+}
