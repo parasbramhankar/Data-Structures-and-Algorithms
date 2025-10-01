a57f8bb51bcf9ab1fb0c2e83acf73b6da0158da3
parasbramhankar <parasbramhankar.in@gmail.com>
2025-10-01 08:27:31 +0530

    Linked List commit

--- src/LinkedList/Practice_Ignore/DoublyLinkedList/Implementation.java
+++ src/LinkedList/Practice_Ignore/DoublyLinkedList/Implementation.java
@@ -1,40 +1,58 @@
 package LinkedList.Practice_Ignore.DoublyLinkedList;
 
-class Node{
-    int data;
-    Node next;
-    Node prev;
-    Node(int data,Node next,Node prev){
-        this.data=data;
-        this.next=null;
-        this.prev=null;
-    }
-    Node(int data){
-        this.data=data;
-    }
-}
+
 public class Implementation {
-    public Node addIntoTheList(int[]arr ) {
-        Node head = new Node(arr[0]);
-        Node temp = head;
+
+    public static Node addIntoTheList(int[]arr ) {
+        Node head=new Node(arr[0]);
+
+        Node temp=head;
+
         for(int i=1;i<arr.length;i++){
             Node newNode=new Node(arr[i],null,temp);
             temp.next=newNode;
-            temp=newNode;
+            temp=temp.next;
         }
+
         return head;
     }
     static void display(Node head){
         Node temp=head;
-        while(temp.next!=null){
+
+        while(temp!=null){
             System.out.print(temp.data+"->");
             temp=temp.next;
         }
-        System.out.print(temp.data);
+        System.out.print("null");
     }
+
+    static Node getTailNode(Node head){
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        return temp;
+    }
+
+    public static void displayBack(Node tail){
+        while(tail!=null){
+            System.out.print(tail.data+"->");
+            tail=tail.prev;
+        }
+
+        System.out.print("null");
+    }
+
     public static void main(String[] args) {
-        int[]arr=new int[]{10,20,30,40,50};
-        Node head=new Implementation().addIntoTheList(arr);
+        int []arr=new int[]{10,20,30,40,50,60,70};
+
+
+        System.out.println("\nFront Traversal:");
+        Node head=addIntoTheList(arr);
         display(head);
+
+        System.out.println("\nBack Traversal:");
+        Node tail=getTailNode(head);
+        displayBack(tail);
     }
 }
