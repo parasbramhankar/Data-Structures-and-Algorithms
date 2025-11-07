6e60ac4763496ff8680a5122cfacb7b211e36d4d
parasbramhankar <parasbramhanakar.in@gmail.com>
2025-11-07 06:22:38 +0530

    Queue commit

---
+++ src/Queue/ImplementUsingArray/Queue.java
@@ -0,0 +1,80 @@
+package Queue.ImplementUsingArray;
+
+import java.util.Scanner;
+
+public class Queue {
+
+    int front;
+    int rear;
+    int []arr;
+    int capacity;
+
+    Queue(int capacity){
+        this.capacity=capacity;
+        arr=new int[capacity];
+        front=-1;
+        rear=-1;
+    }
+
+    void enQueue(int x){
+        if(isFull()){
+            System.out.println("Queue is already full");
+            return;
+        }
+        if(front==-1 && rear==-1){
+            front=0;
+        }
+        rear++;
+        arr[rear]=x;
+    }
+    boolean isEmpty(){
+        return (front==-1 && rear==-1);
+    }
+    boolean isFull(){
+        return (rear==capacity-1);
+    }
+
+    int deQueue(){
+        if(isEmpty()){
+            System.out.println("Deletion not possible");
+        }
+        int ele=arr[front];
+        arr[front]=0;
+        front++;
+        return ele;
+    }
+
+    int peek(){
+        return arr[front];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter the capacity of the the linked list: ");
+        int capacity=scanner.nextInt();
+
+        Queue queue=new Queue(capacity);
+
+        System.out.println("Enter the number of elements: ");
+        int noOfElement= scanner.nextInt();
+
+        if(capacity<noOfElement){
+            System.out.println("capacity is less than the number of elements you want to insert");
+            System.exit(0);
+        }
+
+        for(int i=0;i<noOfElement;i++){
+            queue.enQueue(scanner.nextInt());
+        }
+
+        //peek operation
+        System.out.println("Front Element: "+queue.peek());
+
+        // dequeue operations
+        System.out.println("Dequeue: "+queue.deQueue());
+        System.out.println("Dequeue: "+queue.deQueue());
+
+
+    }
+}
